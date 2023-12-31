ea30ea60ebf9a1d9d1333d955ef11cdb5ba41dbe
Leangeful <cuttheleash1@gmail.com>
2023-12-31 20:00:18 +0100

    chore: nav list

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -3,8 +3,6 @@
 </script>
 
 <nav>
-	<ol>
-		<li><a class="btn btn-sm variant-ghost-surface" href="{base}/"> Home </a></li>
-		<li><a class="btn btn-sm variant-ghost-surface" href="{base}/upgrades"> Upgrades </a></li>
-	</ol>
+	<a class="btn btn-sm variant-ghost-surface" href="{base}/"> Home </a>
+	<a class="btn btn-sm variant-ghost-surface" href="{base}/upgrades"> Upgrades </a>
 </nav>
