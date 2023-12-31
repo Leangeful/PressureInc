3e5c7a7c240f9ce236fde9112bf25c364a4d445c
Leangeful <cuttheleash1@gmail.com>
2023-12-31 19:19:02 +0100

    fix: gh-pages paths

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <nav>
-	<a class="btn btn-sm variant-ghost-surface" href="{base}/"> Home </a>
+	<a class="btn btn-sm variant-ghost-surface" href="/"> Home </a>
 	<a class="btn btn-sm variant-ghost-surface" href="{base}/upgrades"> Upgrades </a>
 </nav>
